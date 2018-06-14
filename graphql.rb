ed9c3bfca01d3fe448ea01d07198c80414f16857
Divyendu Singh <divyendu.z@gmail.com>
2018-06-14 16:07:14 +0200

    update sha

--- graphql.rb
+++ graphql.rb
@@ -2,7 +2,7 @@ class Graphql < Formula
   desc "Command line tool for common GraphQL development workflows"
   homepage "https://github.com/graphql-cli/graphql-cli"
   url "https://github.com/graphql-cli/graphql-cli/releases/download/v2.16.3/graphql-macos"
-  sha256 "091a2d3b4fb626a560b657411d755025298bad06de07406af648dace600dd30a"
+  sha256 "84007207db2722b3f3e7940db30629b646a8e4bcc01324a7661d6bce430cfefa"
   version "2.16.3"
 
   bottle :unneeded
